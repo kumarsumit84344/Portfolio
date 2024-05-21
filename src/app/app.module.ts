09b7ba1495662ce73220c54f6629e6722996da29
kumarsumit84344 <70761681+kumarsumit84344@users.noreply.github.com>
2024-05-21 10:33:51 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TabViewModule } from 'primeng/tabview';
-import { AccordionModule } from 'primeng/accordion';
 import { FormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { DialogModule } from 'primeng/dialog';
-import { ButtonModule } from 'primeng/button';
-import { ChartModule } from 'primeng/chart';
 import { PortfolioComponent } from './Components/portfolio/portfolio.component';
 
 @NgModule({
@@ -19,15 +13,12 @@ import { PortfolioComponent } from './Components/portfolio/portfolio.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TabViewModule,
-    AccordionModule,
+   
     FormsModule,
     InputTextModule,
     HttpClientModule,
     ReactiveFormsModule,
-    DialogModule,
-    ButtonModule,
-    ChartModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent],
